62f07f96b56dbd5361434c019fd1e4c3cd60e6d0
ChemicalLuck <j.tebbett@outlook.com>
2025-05-12 12:03:57 +0100

    Corrected config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   sourcemap: true,
-  minify: false,
+  minify: true,
   dts: true,
   format: ["cjs", "esm"],
   target: "node20",
